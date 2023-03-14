31751d1814563c5c2a3cd4cba74a282c74f3b7ae
Angel Paunov <angel_paunov6@abv.bg>
2023-03-14 19:21:19 +0200

    Create Project And Solution

---
+++ Hotel-Reservation-Manager/Hotel-Reservation-Manager.Web/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hotel_Reservation_Manager.Web.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
