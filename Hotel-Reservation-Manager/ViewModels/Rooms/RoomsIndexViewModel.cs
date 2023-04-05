c613ec89710f32ce41676db542064c7008e068f9
batable245 <iordan.kar2@gmail.com>
2023-04-05 20:45:46 +0300

    Delete Duplicated Foler

--- Hotel-Reservation-Manager/ViewModels/Rooms/RoomsIndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Hotel_Reservation_Manager.ViewModels.Rooms
-{
-    public class RoomsIndexViewModel
-    {
-        public ICollection<RoomIndexViewModel> Rooms { get; set; }
-    }
-}
